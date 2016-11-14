8649b7e3a89332f79fc2e1a112010a08ac619451
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-11-14 16:54:17 -0500

    add partial

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -13,4 +13,42 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.find(params[:id])
   end
+
+  # gets movie based on id value in URL params hash for editing purposes
+  def edit
+    @movie = Movie.find(params[:id])
+  end
+
+  def update
+    @movie = Movie.find(params[:id])
+    @movie.update(movie_params)
+    redirect_to movie_path(@movie)
+  end
+
+  def new
+    @movie = Movie.new
+  end
+
+  def create
+    @movie = Movie.new(movie_params)
+    if @movie.valid?
+      @movie.save
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  def destroy
+    @movie = Movie.find(params[:id])
+    @movie.delete
+    redirect_to movies_path
+  end
+
+  private
+
+  def movie_params
+    params.require(:movie).permit(:title, :rating, :total_gross, :description,
+                                  :released_on)
+  end
 end
