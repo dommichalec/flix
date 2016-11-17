0ad0759ac3db88ac8ec19957291c7b4f4ab63d22
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-11-16 21:48:06 -0500

    add new registration path

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,7 +1,32 @@
 class RegistrationsController < ApplicationController
+  before_action :set_movie, only: [:index, :new, :create]
 
   def index
-    @movie = Movie.find(params[:movie_id])
     @registrations = @movie.registrations
+  end
+
+  def new
+    @registration = @movie.registrations.new
+  end
+
+  def create
+    @registration = @movie.registrations.new(registration_params)
+    if @registration.save
+      flash[:notice] = "Awesome! You're registered for #{@movie.title}!"
+      redirect_to movie_path(@movie)
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def registration_params
+    params.require(:registration).permit(:first_name, :last_name,
+                                         :email, :how_heard)
+  end
+
+  def set_movie
+    @movie = Movie.find(params[:movie_id])
   end
 end
