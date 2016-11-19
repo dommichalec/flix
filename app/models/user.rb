5114a8d21b4e64928278d8411a9742551d48b74a
Dominic Michalec <dominicjjmichalec@gmail.com>
2016-11-19 15:02:18 -0500

    update email validation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password # attr_accessor :password, :password_confirmation
   # associations
 
+  # constants
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
+
   # validations
-  validates :first_name, :last_name
-  validates :email, presence: true,
-    format: /\A\S+@\S\z/, uniqueness: { case_sensitive: false }
+  validates :first_name, :last_name, presence: true
+  validates :email, format: { with: VALID_EMAIL_REGEX },
+  uniqueness: { case_sensitive: false }
 end
